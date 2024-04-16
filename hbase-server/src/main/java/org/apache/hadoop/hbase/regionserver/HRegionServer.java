063d98b67bc2a517a4e6bedb4db1e5bb61439308
Abhishek Kothalikar <99398985+kabhishek4@users.noreply.github.com>
2024-04-16 18:03:06 +0530

    CDPD-65373: HBASE-28292 Make Delay prefetch property to be dynamically configured
    Signed-off-by: Wellington Chevreuil <wchevreuil@apache.org>
    Signed-off-by: Peter Somogyi <psomogyi@apache.org>
    
    (cherry picked from commit 16e9affca37f0027e1bc66e873cb291097aa75dd)
    (cherry picked from commit cbc45c08e5aec9da3f87cfe8f400e94d8054766b)
    Change-Id: I5fb092961de30a7cb96c0ec305b79f8ff1411c92

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionServer.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionServer.java
@@ -586,6 +586,9 @@ public class HRegionServer extends Thread
   // A timer to shutdown the process if abort takes too long
   private Timer abortMonitor;
 
+  // A timer submit requests to the PrefetchExecutor
+  private PrefetchExecutorNotifier prefetchExecutorNotifier;
+
   /**
    * Starts a HRegionServer at the default location.
    * <p/>
@@ -2163,6 +2166,9 @@ public class HRegionServer extends Thread
     // Compaction thread
     this.compactSplitThread = new CompactSplit(this);
 
+    // Prefetch Notifier
+    this.prefetchExecutorNotifier = new PrefetchExecutorNotifier(conf);
+
     // Background thread to check for compactions; needed if region has not gotten updates
     // in a while. It will take care of not checking too frequently on store-by-store basis.
     this.compactionChecker = new CompactionChecker(this, this.compactionCheckFrequency, this);
@@ -2227,6 +2233,7 @@ public class HRegionServer extends Thread
     // Registering the compactSplitThread object with the ConfigurationManager.
     configurationManager.registerObserver(this.compactSplitThread);
     configurationManager.registerObserver(this.rpcServices);
+    configurationManager.registerObserver(this.prefetchExecutorNotifier);
     configurationManager.registerObserver(this);
   }
 
