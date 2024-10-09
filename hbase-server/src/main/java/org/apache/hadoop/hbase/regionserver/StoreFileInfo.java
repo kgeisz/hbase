98976472e236de865aaf338516477851f2b4eb15
Charles Connell <cconnell@hubspot.com>
2024-10-09 04:53:39 -0400

    CDPD-78227: Backport HBASE-28905 to CDH7.3.1.x
    CDPD-75784: Backport HBASE-28905: Skip excessive evaluations of LINK_NAME_PATTERN and REF_NAME_PATTERN regular expressions (#6355)
    
    Signed-off-by: Wellington Chevreuil <wchevreuil@apache.org>
    Change-Id: Ie2857bb98585b7bea5d3c99b4bb5fde827c76e99

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/StoreFileInfo.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/StoreFileInfo.java
@@ -52,7 +52,7 @@ public class StoreFileInfo {
 
   /**
    * A non-capture group, for hfiles, so that this can be embedded. HFiles are uuid ([0-9a-z]+).
-   * Bulk loaded hfiles has (_SeqId_[0-9]+_) has suffix. The mob del file has (_del) as suffix.
+   * Bulk loaded hfiles have (_SeqId_[0-9]+_) as a suffix. The mob del file has (_del) as a suffix.
    */
   public static final String HFILE_NAME_REGEX = "[0-9a-f]+(?:(?:_SeqId_[0-9]+_)|(?:_del))?";
 
@@ -64,7 +64,7 @@ public class StoreFileInfo {
    * hfilelink reference names ({@code
    *
   <table>
-   * =<region>-<hfile>.<parentEncRegion>}) If reference, then the regex has more than just one
+   * =<region>-<hfile>.<parentEncRegion>}). If reference, then the regex has more than just one
    * group. Group 1, hfile/hfilelink pattern, is this file's id. Group 2 '(.+)' is the reference's
    * parent region name.
    */
@@ -472,6 +472,13 @@ public class StoreFileInfo {
    * @return True if the path has format of a HStoreFile reference.
    */
   public static boolean isReference(final String name) {
+    // The REF_NAME_PATTERN regex is not computationally trivial, so see if we can fast-fail
+    // on a simple heuristic first. The regex contains a literal ".", so if that character
+    // isn't in the name, then the regex cannot match.
+    if (!name.contains(".")) {
+      return false;
+    }
+
     Matcher m = REF_NAME_PATTERN.matcher(name);
     return m.matches() && m.groupCount() > 1;
   }
