243920662cfa066338b06981372172b589384307
vinayak hegde <vinayakph123@gmail.com>
2025-06-11 19:55:48 +0530

    HBASE-29255: Integrate backup WAL cleanup logic with the delete command (#7007)
    * Store bulkload files in daywise bucket as well
    
    * Integrate backup WAL cleanup logic with the delete command
    
    * address the review comments
    
    * address the review comments
    
    * address the review comments
    
    * add more unit tests to cover all cases
    
    * address the review comments

--- hbase-backup/src/main/java/org/apache/hadoop/hbase/backup/replication/ContinuousBackupReplicationEndpoint.java
+++ hbase-backup/src/main/java/org/apache/hadoop/hbase/backup/replication/ContinuousBackupReplicationEndpoint.java
@@ -23,6 +23,7 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.util.List;
 import java.util.Map;
+import java.util.TimeZone;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
