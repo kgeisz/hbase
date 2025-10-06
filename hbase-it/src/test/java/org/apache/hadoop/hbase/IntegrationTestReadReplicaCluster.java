ee50a1767837ced6d4184907c4035d2336ed0ba8
Kevin Geiszler <kevin.j.geiszler@gmail.com>
2025-10-06 09:18:55 -0700

    Successfully switch active and replica cluster
    Change-Id: I48b73d4a2a2d1a844abb3a810cdaab270e019663

--- hbase-it/src/test/java/org/apache/hadoop/hbase/IntegrationTestReadReplicaCluster.java
+++ hbase-it/src/test/java/org/apache/hadoop/hbase/IntegrationTestReadReplicaCluster.java
@@ -17,6 +17,7 @@
  */
 package org.apache.hadoop.hbase;
 
+import static org.apache.hadoop.hbase.HConstants.HBASE_CLIENT_RETRIES_NUMBER;
 import static org.apache.hadoop.hbase.HConstants.HBASE_DIR;
 import static org.apache.hadoop.hbase.HConstants.HBASE_GLOBAL_READONLY_ENABLED_KEY;
 import static org.apache.hadoop.hbase.HConstants.HBASE_META_TABLE_SUFFIX;
@@ -36,6 +37,7 @@ import org.apache.hadoop.hbase.procedure2.ProcedureExecutor;
 import org.apache.hadoop.hbase.security.access.ReadOnlyController;
 import org.apache.hadoop.hbase.testclassification.IntegrationTests;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.util.FSUtils;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.slf4j.Logger;
@@ -52,8 +54,8 @@ public class IntegrationTestReadReplicaCluster extends IntegrationTestBase {
   protected static final Logger LOG =
     LoggerFactory.getLogger(IntegrationTestReadReplicaCluster.class);
   protected static final String READ_ONLY_CONTROLLER_NAME = ReadOnlyController.class.getName();
-  protected static final String META_TABLE_SUFFIX = "2";
-  protected static final byte[] TABLE_NAME = Bytes.toBytes("testTable");
+  protected static final String TEST_META_TABLE_SUFFIX = "2";
+  // protected static final byte[] TABLE_NAME = Bytes.toBytes("testTable");
   protected static final byte[] COLUMN_FAMILY = Bytes.toBytes("cf1");
   protected static final byte[] QUALIFIER = Bytes.toBytes("q1");
   protected Configuration activeConf;
@@ -68,7 +70,6 @@ public class IntegrationTestReadReplicaCluster extends IntegrationTestBase {
   Admin replicaAdmin;
   protected ProcedureExecutor<MasterProcedureEnv> replicaProcExecutor;
   Path rootDir;
-  String activeClusterId;
   FileSystem fs;
 
   @Override
@@ -76,7 +77,7 @@ public class IntegrationTestReadReplicaCluster extends IntegrationTestBase {
     LOG.info("kevin: start setUpCluster");
 
     // Set up and start the active cluster
-    util = new IntegrationTestingUtility();  // The test fails if util is not set
+    util = new IntegrationTestingUtility(); // The test fails if util is not set
     activeUtil = util;
     activeConf = activeUtil.getConfiguration();
     activeConf.setBoolean(HBASE_GLOBAL_READONLY_ENABLED_KEY, false);
@@ -86,19 +87,23 @@ public class IntegrationTestReadReplicaCluster extends IntegrationTestBase {
     // Minimize resource contention within the DFS
     activeConf.setInt("dfs.datanode.handler.count", 1);
     activeConf.setInt("dfs.namenode.handler.count", 1);
+    // Prevent retries for Puts on the replica cluster that are expected to fail
+    activeConf.setInt(HBASE_CLIENT_RETRIES_NUMBER, 0);
+    // activeConf.setInt("dfs.socket.timeout", 180*1000);
+    // activeConf.setInt("dfs.datanode.socket.write.timeout", 180*1000);
+    // activeConf.setInt("dfs.client-write-packet-timeout", 120*1000);
+
     LOG.info("kevin: starting cluster1 minicluster");
     activeCluster = activeUtil.startMiniCluster();
     String rootDir1 = activeCluster.getConfiguration().get(HBASE_DIR);
     LOG.info("kevin: finished starting cluster1 minicluster");
-    activeClusterId = activeCluster.getMaster().getClusterId();
-    LOG.info("kevin: active cluster's ID = {}", activeClusterId);
     activeConn = activeUtil.getConnection();
     activeAdmin = activeConn.getAdmin();
 
     // Use the active cluster's existing configuration to set up and start the replica cluster
     replicaConf = HBaseConfiguration.create(activeConf);
     replicaConf.setBoolean(HBASE_GLOBAL_READONLY_ENABLED_KEY, true);
-    replicaConf.set(HBASE_META_TABLE_SUFFIX, META_TABLE_SUFFIX);
+    replicaConf.set(HBASE_META_TABLE_SUFFIX, TEST_META_TABLE_SUFFIX);
     replicaUtil = new IntegrationTestingUtility(replicaConf);
     replicaUtil.setDataTestDirOnTestFS(activeUtil.getDataTestDirOnTestFS());
     replicaUtil.setDFSCluster(activeUtil.getDFSCluster());
@@ -106,16 +111,22 @@ public class IntegrationTestReadReplicaCluster extends IntegrationTestBase {
     replicaCluster = replicaUtil.startMiniCluster();
     String rootDir2 = replicaCluster.getConfiguration().get(HBASE_DIR);
     LOG.info("kevin: finished starting cluster2 minicluster");
-    LOG.info("kevin: replica cluster's ID = {}", replicaCluster.getMaster().getClusterId());
     replicaConn = replicaUtil.getConnection();
     replicaAdmin = replicaConn.getAdmin();
     replicaProcExecutor = replicaUtil.getHBaseCluster().getMaster().getMasterProcedureExecutor();
 
+    LOG.info("kevin: active cluster ID = {}", activeCluster.getMaster().getClusterId());
+    LOG.info("kevin: replica cluster ID = {}", replicaCluster.getMaster().getClusterId());
+    // assertNotEquals(activeCluster.getMaster().getClusterId(),
+    // replicaCluster.getMaster().getClusterId());
+
     fs = activeCluster.getMaster().getFileSystem();
     assertProperInitialization(rootDir1, rootDir2);
 
-    LOG.info("kevin: dfs.datanode.handler.count = {}", activeConf.get("dfs.datanode.handler.count"));
-    LOG.info("kevin: dfs.namenode.handler.count = {}", activeConf.get("dfs.namenode.handler.count"));
+    LOG.info("kevin: dfs.datanode.handler.count = {}",
+      activeConf.get("dfs.datanode.handler.count"));
+    LOG.info("kevin: dfs.namenode.handler.count = {}",
+      activeConf.get("dfs.namenode.handler.count"));
 
     LOG.info("kevin: end setUpCluster");
   }
@@ -168,97 +179,137 @@ public class IntegrationTestReadReplicaCluster extends IntegrationTestBase {
     assertTrue("Expected " + MasterRegionFactory.MASTER_STORE_DIR + " to exist in the filesystem",
       fs.exists(new Path(rootDir, MasterRegionFactory.MASTER_STORE_DIR)));
     assertTrue(
-      "Expected " + MasterRegionFactory.MASTER_STORE_DIR + "_" + META_TABLE_SUFFIX
+      "Expected " + MasterRegionFactory.MASTER_STORE_DIR + "_" + TEST_META_TABLE_SUFFIX
         + " to exist in the filesystem",
-      fs.exists(new Path(rootDir, MasterRegionFactory.MASTER_STORE_DIR + "_" + META_TABLE_SUFFIX)));
+      fs.exists(
+        new Path(rootDir, MasterRegionFactory.MASTER_STORE_DIR + "_" + TEST_META_TABLE_SUFFIX)));
+
+    validateActiveClusterSuffixFile(activeCluster.getMaster().getClusterId(), "");
+  }
+
+  // Checks for the correct active cluster ID and suffix in the active cluster file
+  public void validateActiveClusterSuffixFile(String clusterId, String suffix) throws IOException {
+    ActiveClusterSuffix activeClusterSuffix = FSUtils.getActiveClusterSuffix(fs, rootDir);
+    String expectedIdAndSuffix = clusterId + ":" + suffix;
+    assertEquals("Expected the active cluster file to have the following cluster ID and "
+      + "suffix: " + expectedIdAndSuffix, expectedIdAndSuffix,
+      activeClusterSuffix.getActiveClusterSuffix());
   }
 
   @Test
   public void testReadReplicaCluster() throws IOException, InterruptedException {
+    LOG.info("kevin: start of testReadReplicaCluster()");
     // Create a table. The replica cluster won't have this table until refresh_meta is run.
-    attemptCreateOnReplicaCluster();
-    createTableOnActiveCluster();
-      assertArrayEquals("The active cluster should have a table called " + Arrays.toString(TABLE_NAME),
-              TABLE_NAME, Arrays.stream(activeConn.getAdmin().listTableNames()).toList().get(0).getName());
+    final byte[] table1 = Bytes.toBytes("testTable1");
+    attemptCreateOnReplicaCluster(replicaUtil, table1);
+    createTableOnActiveCluster(activeUtil, table1);
+    assertArrayEquals("The active cluster should have a table called " + Arrays.toString(table1),
+      table1, Arrays.stream(activeConn.getAdmin().listTableNames()).toList().get(0).getName());
     assertTrue("The read replica cluster should not have any tables yet",
-            Arrays.stream(replicaConn.getAdmin().listTableNames()).toList().isEmpty());
+      Arrays.stream(replicaConn.getAdmin().listTableNames()).toList().isEmpty());
     refreshMeta();
-    assertFalse("The replica cluster should now have a table called " + Arrays.toString(TABLE_NAME),
+    assertFalse("The replica cluster should now have a table called " + Arrays.toString(table1),
       Arrays.stream(replicaConn.getAdmin().listTableNames()).toList().isEmpty());
 
     // Add data to the active cluster
     byte[] row1 = Bytes.toBytes("row1");
-    try (Table activeTable = activeConn.getTable(TableName.valueOf(TABLE_NAME))) {
+    try (Table activeTable = activeConn.getTable(TableName.valueOf(table1))) {
       // Add data to the active cluster
       Put put = new Put(row1);
       put.addColumn(COLUMN_FAMILY, QUALIFIER, Bytes.toBytes("1"));
       activeTable.put(put);
     }
 
-    Result result = getRowFromActiveCluster(row1);
-    assertFalse("The Get result should not be empty since data was inserted for row " + Arrays.toString(row1),
-      result.isEmpty());
-    activeAdmin.flush(TableName.valueOf(TABLE_NAME));
+    // Verify new data cannot be put on the replica cluster
+    attemptPutOnReplicaCluster(replicaUtil, table1);
 
-    result = getRowFromReplicaCluster(row1);
-    assertTrue("The Get result should be empty since the replica cluster has not been refreshed",
+    Result result = getRow(activeConn, table1, row1);
+    assertFalse("The Get result should not be empty on the active cluster since data was inserted "
+      + "for row " + Arrays.toString(row1), result.isEmpty());
+    activeAdmin.flush(TableName.valueOf(table1));
+
+    result = getRow(replicaConn, table1, row1);
+    assertTrue(
+      "The Get result should be empty on the replica cluster since it has not been refreshed",
       result.isEmpty());
 
     refreshMeta();
     refreshHFiles();
 
-    result = getRowFromReplicaCluster(row1);
-    assertFalse("The Get result should be not empty since the replica cluster has been refreshed",
+    result = getRow(replicaConn, table1, row1);
+    assertFalse(
+      "The Get result should not be empty on the replica cluster since it has been refreshed",
       result.isEmpty());
 
+    // TODO - switch the active cluster and the replica cluster
 
+    // Dynamically switch the active cluster to read-only mode and verify it no longer supports
+    // puts and table creations
+    activeConf.setBoolean(HBASE_GLOBAL_READONLY_ENABLED_KEY, true);
+    activeUtil.notifyConfigurationObservers(activeCluster);
+    attemptPutOnReplicaCluster(activeUtil, table1);
+    final byte[] table2 = Bytes.toBytes("testTable2");
+    attemptCreateOnReplicaCluster(activeUtil, table2);
 
-    // TODO run create table and then refresh meta
-    // What happens if i run refresh_meta and not refresh_hfiles
-    // does the table apperar?
-    // play around with doing on or the other
+    replicaConf.setBoolean(HBASE_GLOBAL_READONLY_ENABLED_KEY, false);
+    replicaUtil.notifyConfigurationObservers(replicaCluster);
+    createTableOnActiveCluster(replicaUtil, table2);
 
-
-
-
-
-
-
-//    try (Table replicaTable = replicaConn.getTable(TableName.valueOf(TABLE_NAME))) {
-//      Get get = new Get(row1);
-//      get.addColumn(COLUMN_FAMILY, QUALIFIER);
-//      Result result = replicaTable.get(get);
-//      LOG.info("kevin: replica cluster get result = {}", result);
-//      String s = "e";
-//    }
-
-
-
+    // try (Table replicaTable = replicaConn.getTable(TableName.valueOf(TABLE_NAME))) {
+    // Get get = new Get(row1);
+    // get.addColumn(COLUMN_FAMILY, QUALIFIER);
+    // Result result = replicaTable.get(get);
+    // LOG.info("kevin: replica cluster get result = {}", result);
+    // String s = "e";
+    // }
 
     String s = "e";
   }
 
-  void attemptCreateOnReplicaCluster() {
-    // This createTable() should throw an exception and take us to the catch block
-    try (Table ignored = replicaUtil.createTable(TableName.valueOf(TABLE_NAME), COLUMN_FAMILY)) {
-      fail("An IOException should have occurred when trying to create a new table on the replica cluster");
+  boolean isReplicaClusterUtil(IntegrationTestingUtility util) {
+    return Boolean.parseBoolean(util.getConfiguration().get(HBASE_GLOBAL_READONLY_ENABLED_KEY));
+  }
+
+  void assertIsReplicaClusterUtil(IntegrationTestingUtility util) {
+    assertTrue(
+      "Expected " + IntegrationTestingUtility.class.getName() + " object from a replica cluster",
+      isReplicaClusterUtil(util));
+  }
+
+  void assertIsActiveClusterUtil(IntegrationTestingUtility util) {
+    assertFalse(
+      "Expected " + IntegrationTestingUtility.class.getName() + " object from an active cluster",
+      isReplicaClusterUtil(util));
+  }
+
+  void attemptCreateOnReplicaCluster(IntegrationTestingUtility util, byte[] tableName) {
+    assertIsReplicaClusterUtil(util);
+    // This createTable() call should throw an exception and take us to the catch block
+    // fail() is supposed to not get executed
+    try (Table ignored = createTable(util, tableName)) {
+      fail(
+        "An IOException should have occurred when trying to create a new table on the replica cluster");
     } catch (IOException e) {
       String expectedMsg = "Operation not allowed in Read-Only Mode";
-      assertTrue("Expected an IOException with the following message: "
-        + expectedMsg, e.getMessage().contains(expectedMsg));
+      assertTrue("Expected an IOException with the following message: " + expectedMsg,
+        e.getMessage().contains(expectedMsg));
+      LOG.info("Successfully received an IOException when trying to create table '{}' on a "
+        + "replica cluster", tableName);
     }
   }
 
-  Table createTable(IntegrationTestingUtility util) throws IOException {
-    try (Table table = util.createTable(TableName.valueOf(TABLE_NAME), COLUMN_FAMILY)) {
+  Table createTable(IntegrationTestingUtility util, byte[] tableName) throws IOException {
+    try (Table table = util.createTable(TableName.valueOf(tableName), COLUMN_FAMILY)) {
       return table;
     }
   }
 
-  void createTableOnActiveCluster() throws IOException, InterruptedException {
+  void createTableOnActiveCluster(IntegrationTestingUtility util, byte[] tableName)
+    throws IOException, InterruptedException {
     LOG.info("kevin: start createTableOnActiveCluster()");
-    try (Table ignored = activeUtil.createTable(TableName.valueOf(TABLE_NAME), COLUMN_FAMILY)) {
-      activeUtil.waitTableAvailable(TableName.valueOf(TABLE_NAME));
+    assertIsActiveClusterUtil(util);
+    try (Table ignored = createTable(util, tableName)) {
+      util.waitTableAvailable(TableName.valueOf(tableName));
     }
     LOG.info("kevin: end createTableOnActiveCluster()");
   }
@@ -277,8 +328,8 @@ public class IntegrationTestReadReplicaCluster extends IntegrationTestBase {
     LOG.info("kevin: end replicaAdmin.refreshHFiles()");
   }
 
-  Result getRow(Connection conn, byte[] row) throws IOException {
-    try (Table table = conn.getTable(TableName.valueOf(TABLE_NAME))) {
+  Result getRow(Connection conn, byte[] tableName, byte[] row) throws IOException {
+    try (Table table = conn.getTable(TableName.valueOf(tableName))) {
       // Verify no data was added to the replica cluster
       Get get = new Get(row);
       get.addColumn(COLUMN_FAMILY, QUALIFIER);
@@ -286,12 +337,31 @@ public class IntegrationTestReadReplicaCluster extends IntegrationTestBase {
     }
   }
 
-  Result getRowFromActiveCluster(byte[] row) throws IOException {
-    return getRow(activeConn, row);
+  Result getRowFromActiveCluster(byte[] tableName, byte[] row) throws IOException {
+    return getRow(activeConn, tableName, row);
   }
 
-  Result getRowFromReplicaCluster(byte[] row) throws IOException {
-    return getRow(replicaConn, row);
+  Result getRowFromReplicaCluster(byte[] tableName, byte[] row) throws IOException {
+    return getRow(replicaConn, tableName, row);
+  }
+
+  void attemptPutOnReplicaCluster(IntegrationTestingUtility util, byte[] tableName) {
+    LOG.info("kevin: start attemptPutOnReplicaCluster()");
+    assertIsReplicaClusterUtil(util);
+    byte[] row = Bytes.toBytes("impossiblePutRow");
+    try (Table replicaTable = util.getConnection().getTable(TableName.valueOf(tableName))) {
+      // Add data to the active cluster
+      Put put = new Put(row);
+      put.addColumn(COLUMN_FAMILY, QUALIFIER, Bytes.toBytes("impossibleValue"));
+      replicaTable.put(put);
+      fail("An IOException should have occurred when trying to perform a Put on the '"
+        + Arrays.toString(tableName) + "' table in the replica cluster");
+    } catch (IOException e) {
+      String expectedMsg = "Operation not allowed in Read-Only Mode";
+      assertTrue("Expected an IOException with the following message: " + expectedMsg,
+        e.getMessage().contains(expectedMsg));
+    }
+    LOG.info("kevin: end attemptPutOnReplicaCluster()");
   }
 
   @Override
