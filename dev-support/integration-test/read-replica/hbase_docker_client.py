f0794db924c7df224083de4189f2e0e4e8e1e6bc
Kevin Geiszler <kevin.j.geiszler@gmail.com>
2026-04-13 10:16:13 -0700

    Add test_table_creation_behavior.py
    Change-Id: I6804343249799e413289d7a5b7112150d7d68e75

--- dev-support/integration-test/read-replica/hbase_docker_client.py
+++ dev-support/integration-test/read-replica/hbase_docker_client.py
@@ -6,9 +6,6 @@ import subprocess
 import time
 import requests
 
-from dotenv import load_dotenv
-from environment_loader import get_env
-
 logger = logging.getLogger(__name__)
 
 
@@ -156,93 +153,3 @@ class HBaseDockerClient:
             logger.info(f"Running 'refresh_meta' on {replica_cluster.name} cluster to sync it with "
                         f"the {active_cluster.name}")
             replica_cluster.refresh_meta()
-
-
-if __name__ == "__main__":
-    logging.basicConfig(format='%(asctime)s %(levelname)-5s %(module)s.%(funcName)s(%(lineno)d): '
-                               '%(message)s',
-                        level=logging.INFO)
-    load_dotenv()
-    container_name = get_env("HBASE_CONTAINER_NAME")
-    table_name = "t1"
-    column_family = "cf"
-
-    active_cluster = HBaseDockerClient(container_name=container_name,
-                                       hbase_ui_port=get_env('ACTIVE_CLUSTER_PORT'),
-                                       cluster_name="Active Cluster")
-    replica_cluster = HBaseDockerClient(container_name=f"{container_name}-2",
-                                        hbase_ui_port=get_env('REPLICA_CLUSTER_PORT'),
-                                        cluster_name="Read-Replica Cluster")
-    try:
-        # Delete any lingering tables
-        logger.info(f"Checking if table '{table_name}' already exists on {active_cluster.name} "
-                    f"and dropping it if necessary")
-        HBaseDockerClient.clean_up_tables(active_cluster, replica_cluster)
-
-        active_cluster.create_table(table_name, column_family)
-
-        # Read-Replica cluster should not see the newly created table yet
-        logger.info(f"Verifying {active_cluster.name} now has '{table_name}', "
-                    f"while {replica_cluster.name} cluster does not")
-        assert active_cluster.verify_table_exists(table_name), \
-            f"Expected table '{table_name}' to exist on {active_cluster.name}"
-        assert not replica_cluster.verify_table_exists(table_name), \
-            f"Table '{table_name}' should not exist on {replica_cluster.name}"
-
-        # Read-Replica cluster should now see the newly created table
-        replica_cluster.refresh_meta()
-        logger.info(f"Verifying {replica_cluster.name} has '{table_name}' after refreshing meta")
-        assert replica_cluster.verify_table_exists(table_name), \
-            (f"Expected table '{table_name}' to exist on {replica_cluster.name} "
-             f"after running refresh_meta")
-        assert active_cluster.verify_table_exists(table_name), \
-            (f"Expected table '{table_name}' to exist on {active_cluster.name} "
-             f"after running refresh_meta")
-
-        # Cannot drop the table on the Read-Replica cluster. A DoNotRetryIOException should occur
-        logger.info(f"Verifying {replica_cluster.name} cannot drop '{table_name}' since it is in "
-                    f"read-only mode")
-        replica_cluster.disable_table(table_name)
-        try:
-            # This should throw an exception
-            replica_cluster.drop_table(table_name)
-
-            # If we get here, then the table was dropped on the read-replica cluster, which should
-            # not have happened.
-            raise RuntimeError(f"Expected trying to drop table '{table_name}' on the "
-                               f"{replica_cluster.name} to result in an error")
-        except HBaseShellCommandError as e:
-            expected_error = ("org.apache.hadoop.hbase.DoNotRetryIOException: "
-                              "Operation not allowed in Read-Only Mode")
-            assert expected_error in str(e), (f"Expected exception to contain the following: "
-                                              f"{expected_error}\n"
-                                              f"The actual exception was:\n{e}")
-        # The table should still exist on the read-replica cluster since drops are not allowed
-        assert replica_cluster.verify_table_exists(table_name), \
-            (f"Expected table '{table_name}' to still exist on {replica_cluster.name} "
-             f"after drop attempt")
-
-        logger.info(f"Dropping table '{table_name}' on {active_cluster.name}")
-        active_cluster.disable_table(table_name)
-        active_cluster.drop_table(table_name)
-
-        # The read-replica cluster should still have the table that was dropped on the active
-        # cluster since 'refresh_meta' has not been run yet.
-        logger.info(f"Verifying {replica_cluster.name} still has table '{table_name}'")
-        assert not active_cluster.verify_table_exists(table_name), \
-            f"Expected table '{table_name}' to have been dropped on {active_cluster.name}"
-        assert replica_cluster.verify_table_exists(table_name), \
-            (f"Table '{table_name}' should still exist on {replica_cluster.name} "
-             f"since 'refresh_meta' has not been run again")
-
-        # The read-replica cluster no longer has the dropped table after running 'refresh_meta'.
-        logger.info(f"Verifying {replica_cluster.name} no longer has table '{table_name}' after "
-                    f"refreshing meta")
-        replica_cluster.refresh_meta()
-        assert not replica_cluster.verify_table_exists(table_name), \
-            (f"Expected table '{table_name}' no longer exist on {replica_cluster.name} "
-             f"after running 'refresh_meta'")
-    except (RuntimeError, HBaseShellCommandError, KeyboardInterrupt) as e:
-        logger.error(f"An error occurred:\n{e}")
-        logger.info("Cleaning up any tables that may be remaining")
-        HBaseDockerClient.clean_up_tables(active_cluster, replica_cluster)
