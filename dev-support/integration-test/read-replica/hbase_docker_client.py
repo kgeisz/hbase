59196b8bd46b52a88ac5766571a163656a0c2e4c
Kevin Geiszler <kevin.j.geiszler@gmail.com>
2026-04-13 08:52:59 -0700

    Add log messages
    Change-Id: I1fab4cdf422dbc267e573a43878651de3af8b29d

--- dev-support/integration-test/read-replica/hbase_docker_client.py
+++ dev-support/integration-test/read-replica/hbase_docker_client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import ast
+import logging
 import re
 import subprocess
 import time
@@ -7,6 +8,8 @@ import requests
 
 from dotenv import load_dotenv
 from environment_loader import get_env
+
+logger = logging.getLogger(__name__)
 
 
 class HBaseShellCommandError(Exception):
@@ -29,17 +32,17 @@ class HBaseDockerClient:
     def wait_for_hbase_ui(self):
         """Checks for a 200 OK on the HBase Master UI."""
         url = f"http://localhost:{self._hbase_ui_port}"
-        print(f"--- Waiting for HBase UI: {self._cluster_name} on {url} ---")
+        logger.info(f"--- Waiting for HBase UI: {self._cluster_name} on {url} ---")
         last_exception = None
         for attempt in range(1, self._max_retries + 1):
             try:
                 response = requests.get(url)
                 if response.status_code == 200:
-                    print(f"SUCCESS: {self._cluster_name} UI is up.")
+                    logger.info(f"SUCCESS: {self._cluster_name} UI is up.")
                     return True
             except requests.exceptions.ConnectionError as e:
                 last_exception = e
-                print(".", end="", flush=True)
+            logging.info(f"Waiting {self._sleep_time} seconds before requesting HBase UI again")
             time.sleep(self._sleep_time)
 
         raise RuntimeError(f"\nTIMEOUT: {self._cluster_name} UI failed to respond after "
@@ -48,7 +51,7 @@ class HBaseDockerClient:
 
     def check_server_status(self):
         """Runs 'status' inside the HBase shell and validates the output."""
-        print(f"--- Validating Cluster Status: {self._cluster_name} ({self._container_name}) ---")
+        logger.info(f"--- Validating Cluster Status: {self._cluster_name} ({self._container_name}) ---")
         for attempt in range(1, self._max_retries + 1):
             try:
                 output = self.get_hbase_status()
@@ -63,17 +66,19 @@ class HBaseDockerClient:
 
                 if all(validations.values()):
                     for check, status in validations.items():
-                        print(f"    [PASS] {check}")
-                    print(f"SUCCESS: {self._cluster_name} is fully operational.")
+                        logger.info(f"    [PASS] {check}")
+                    logger.info(f"SUCCESS: {self._cluster_name} is fully operational.")
                     return True
                 else:
-                    print(f"\nWARN: {self._cluster_name} is responding, but not all "
-                          f"components are ready...")
-                    print(f"HBase 'status' command output: {output}")
+                    logger.warning(f"{self._cluster_name} is responding, but not all "
+                                   f"components are ready...")
+                    logger.info(f"HBase 'status' command output:\n{output}")
 
-            except subprocess.CalledProcessError as e:
-                print(".", end="", flush=True)
+            except subprocess.CalledProcessError:
+                pass
 
+            logging.info(f"Waiting {self._sleep_time} seconds before getting status on "
+                         f"{self.name} again")
             time.sleep(self._sleep_time)
 
         raise RuntimeError(
@@ -86,7 +91,7 @@ class HBaseDockerClient:
                f'''hbase shell -n <<< "{hbase_cmd}"''']
         cmd_str = ' '.join(cmd)
 
-        print(f"Running command on {self._cluster_name}: {cmd_str}")
+        logger.debug(f"Running command on {self._cluster_name}: {cmd_str}")
         process = subprocess.run(cmd, capture_output=True)
         stdout = process.stdout.decode('utf-8')
         if process.returncode != 0:
@@ -99,35 +104,42 @@ class HBaseDockerClient:
         return stdout
 
     def create_table(self, table_name, column_family):
+        logger.info(f"Creating table '{table_name}' on {self._cluster_name}")
         create_cmd = f"create '{table_name}', '{column_family}'"
         output = self.__run_hbase_command(create_cmd)
 
         if f"Created table {table_name}" not in output:
-            print(f"ERROR: Could not create table {table_name} on {self._cluster_name}")
+            logger.error(f"Could not create table '{table_name}' on {self._cluster_name}")
             return False
-        print(f"Created table {table_name} on {self._cluster_name}")
+        logger.info(f"Created table '{table_name}' on {self._cluster_name}")
         return True
 
     def list_tables(self):
         """Gets the list of HBase tables and returns it as a Python list"""
+        logger.debug(f"Getting the list of tables in HBase on {self.name}")
         pattern = r'\[(.*?)\]'
         output = self.__run_hbase_command("list")
         match = re.search(pattern, output)
         return ast.literal_eval(match.group(0))
 
     def verify_table_exists(self, table_name):
+        logger.debug(f"Verifying '{table_name}' is in the list of tables on {self.name}")
         return table_name in self.list_tables()
 
     def get_hbase_status(self):
+        logger.debug(f"Getting status of {self.name}")
         return self.__run_hbase_command("status")
 
     def disable_table(self, table_name):
+        logger.debug(f"Disabling table '{table_name}' on {self.name}")
         self.__run_hbase_command(f"disable '{table_name}'")
 
     def drop_table(self, table_name):
+        logger.debug(f"Dropping table '{table_name}' on {self.name}")
         self.__run_hbase_command(f"drop '{table_name}'")
 
     def refresh_meta(self):
+        logger.debug(f"Refreshing meta on {self.name}")
         self.__run_hbase_command("refresh_meta")
 
     @staticmethod
@@ -138,16 +150,19 @@ class HBaseDockerClient:
         """
         tables = active_cluster.list_tables()
         if tables:
-            print(f"Removing all existing tables on the {active_cluster.name}: {tables}")
+            logger.info(f"Removing all existing tables on {active_cluster.name}: {tables}")
             for table in tables:
                 active_cluster.disable_table(table)
                 active_cluster.drop_table(table)
-            print(f"Running 'refresh_meta' on the {replica_cluster.name} cluster to sync it with "
-                  f"the {active_cluster.name}")
+            logger.info(f"Running 'refresh_meta' on {replica_cluster.name} cluster to sync it with "
+                        f"the {active_cluster.name}")
             replica_cluster.refresh_meta()
 
 
 if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s %(module)s.%(funcName)s(%(lineno)d) '
+                               '%(levelname)s: %(message)s',
+                        level=logging.DEBUG)
     load_dotenv()
     container_name = get_env("HBASE_CONTAINER_NAME")
     table_name = "t1"
@@ -160,26 +175,32 @@ if __name__ == "__main__":
                                         hbase_ui_port=get_env('REPLICA_CLUSTER_PORT'),
                                         cluster_name="Read-Replica Cluster")
     try:
+        # Delete any lingering tables
         HBaseDockerClient.clean_up_tables(active_cluster, replica_cluster)
 
         active_cluster.create_table(table_name, column_family)
 
         # Read-Replica cluster should not see the newly created table yet
+        logger.info(f"Verifying {active_cluster.name} now has '{table_name}', "
+                    f"while {replica_cluster.name} cluster does not")
         assert active_cluster.verify_table_exists(table_name), \
-            f"Expected table '{table_name}' to exist on the {active_cluster.name}"
+            f"Expected table '{table_name}' to exist on {active_cluster.name}"
         assert not replica_cluster.verify_table_exists(table_name), \
-            f"Table '{table_name}' should not exist on the {replica_cluster.name}"
+            f"Table '{table_name}' should not exist on {replica_cluster.name}"
 
         # Read-Replica cluster should now see the newly created table
         replica_cluster.refresh_meta()
+        logger.info(f"Verifying {replica_cluster.name} has '{table_name}' after refreshing meta")
         assert replica_cluster.verify_table_exists(table_name), \
-            (f"Expected table '{table_name}' to exist on the {replica_cluster.name} "
+            (f"Expected table '{table_name}' to exist on {replica_cluster.name} "
              f"after running refresh_meta")
         assert active_cluster.verify_table_exists(table_name), \
-            (f"Expected table '{table_name}' to exist on the {active_cluster.name} "
+            (f"Expected table '{table_name}' to exist on {active_cluster.name} "
              f"after running refresh_meta")
 
         # Cannot drop the table on the Read-Replica cluster. A DoNotRetryIOException should occur
+        logger.info(f"Verifying {replica_cluster.name} cannot drop '{table_name}' since it is in"
+                    f"read-only mode")
         replica_cluster.disable_table(table_name)
         try:
             # This should throw an exception
@@ -195,17 +216,18 @@ if __name__ == "__main__":
             assert expected_error in str(e), (f"Expected exception to contain the following: "
                                               f"{expected_error}\n"
                                               f"The actual exception was:\n{e}")
-
         # The table should still exist on the read-replica cluster since drops are not allowed
         assert replica_cluster.verify_table_exists(table_name), \
-            (f"Expected table '{table_name}' to still exist on the {replica_cluster.name} "
+            (f"Expected table '{table_name}' to still exist on {replica_cluster.name} "
              f"after drop attempt")
 
+        logger.info(f"Dropping table '{table_name}' on {active_cluster.name}")
         active_cluster.disable_table(table_name)
         active_cluster.drop_table(table_name)
 
         # The read-replica cluster should still have the table that was dropped on the active
         # cluster since 'refresh_meta' has not been run yet.
+        logger.info(f"Verifying {replica_cluster.name} still has table '{table_name}'")
         assert not active_cluster.verify_table_exists(table_name), \
             f"Expected table '{table_name}' to have been dropped on {active_cluster.name}"
         assert replica_cluster.verify_table_exists(table_name), \
@@ -213,11 +235,13 @@ if __name__ == "__main__":
              f"since 'refresh_meta' has not been run again")
 
         # The read-replica cluster no longer has the dropped table after running 'refresh_meta'.
+        logger.info(f"Verifying {replica_cluster.name} no longer has table '{table_name}' after "
+                    f"refreshing meta")
         replica_cluster.refresh_meta()
         assert not replica_cluster.verify_table_exists(table_name), \
             (f"Expected table '{table_name}' no longer exist on {replica_cluster.name} "
              f"after running 'refresh_meta'")
-    except (RuntimeError, HBaseShellCommandError) as e:
-        print(f"An error occurred:\n{e}")
-        print("Cleaning up any tables that may be remaining")
+    except (RuntimeError, HBaseShellCommandError, KeyboardInterrupt) as e:
+        logger.error(f"An error occurred:\n{e}")
+        logger.info("Cleaning up any tables that may be remaining")
         HBaseDockerClient.clean_up_tables(active_cluster, replica_cluster)
