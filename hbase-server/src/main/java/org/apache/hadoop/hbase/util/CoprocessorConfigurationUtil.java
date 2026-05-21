b1a2f27a8f8b7649df7aec3bd7efc375be3957c7
Kevin Geiszler <kevin.j.geiszler@gmail.com>
2026-05-20 19:38:03 -0700

    Set coprocessor conf list with empty string instead of using conf.unset() in CoprocessorConfigurationUtil.updateCoprocessorListInConf()
    Change-Id: Ibcd7bd906226209b8e3274c9c9d5a8c0b9ee9d5d

--- hbase-server/src/main/java/org/apache/hadoop/hbase/util/CoprocessorConfigurationUtil.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/util/CoprocessorConfigurationUtil.java
@@ -239,7 +239,7 @@ public final class CoprocessorConfigurationUtil {
     if (updatedCoprocessorList != null) {
       currentConf.setStrings(coprocessorConfKey, updatedCoprocessorList);
     } else {
-      currentConf.unset(coprocessorConfKey);
+      currentConf.set(coprocessorConfKey, "");
     }
 
     LOG.info("kevin: {} {}: After update: currentConf.getStrings({}) = {}",
