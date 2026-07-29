46936f9536ba64bd59791b45849e887689977f77
Kevin Geiszler <kevin.j.geiszler@gmail.com>
2026-07-29 11:22:12 -0400

    Edit read-replica-integration-tests.yml and add python/scripts/new_file.py
    Change-Id: I1fa81179c1d112819c55b6cb58c94e4bdedc0341

---
+++ dev-support/integration-test/read-replica/python/scripts/new_file.py
@@ -0,0 +1,6 @@
+from python.src.logger_config import get_logger
+
+logger = get_logger(__name__)
+
+print("Hello, this is a print() statement inside of a new Python file")
+logger.info("Hello, this is an INFO log message inside of a new Python file")
