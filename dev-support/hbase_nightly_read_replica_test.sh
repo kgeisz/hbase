d39c682c1e8834f0a1611b77884b557eccf51202
Kevin Geiszler <kevin.j.geiszler@gmail.com>
2026-08-28 13:14:00 -0400

    Copy hbase logs to output dir on cleanup in hbase_nightly_read_replica_test.sh
    Change-Id: I7fe38a03de03583a38561f1e2fee20bd2376f9de

--- dev-support/hbase_nightly_read_replica_test.sh
+++ dev-support/hbase_nightly_read_replica_test.sh
@@ -40,7 +40,9 @@ set +a
 
 echo "HBASE_IMAGE=${HBASE_IMAGE}"
 echo "ACTIVE_CLUSTER_CONF_DIR=${ACTIVE_CLUSTER_CONF_DIR}"
+echo "ACTIVE_CLUSTER_LOGS_DIR=${ACTIVE_CLUSTER_LOGS_DIR}"
 echo "REPLICA_CLUSTER_CONF_DIR=${REPLICA_CLUSTER_CONF_DIR}"
+echo "REPLICA_CLUSTER_LOGS_DIR=${REPLICA_CLUSTER_LOGS_DIR}"
 echo "DOCKER_COMPOSE_FILE=${DOCKER_COMPOSE_FILE}"
 echo "HBASE_DATA_STORE_ROOT=${HBASE_DATA_STORE_ROOT}"
 echo "realpath of HBASE_DATA_STORE_ROOT=$(realpath ${HBASE_DATA_STORE_ROOT})"
@@ -63,6 +65,9 @@ cleanup() {
       docker logs "${HBASE_CONTAINER_NAME}-2" 2>&1 || true
     fi
   fi
+  echo "=== Cleanup: Copying HBase logs to ${OUTPUT_DIR} ==="
+  cp "${ACTIVE_CLUSTER_LOGS_DIR}/*"  "${OUTPUT_DIR}"
+  cp "${REPLICA_CLUSTER_LOGS_DIR}/*" "${OUTPUT_DIR}"
   echo "=== Cleanup: Stopping Docker containers ==="
   docker compose -f "${DOCKER_COMPOSE_FILE}" down 2>/dev/null || true
   echo "=== Cleanup: Removing Docker image: ${HBASE_IMAGE} ==="
